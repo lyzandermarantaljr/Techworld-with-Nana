d97d4053dd178b7d2f9cc0c0c735832504bcd034
lyzandermarantaljr <lyzandermarantaljr@gmail.com>
2026-08-03 16:20:50 -0400

    Create app.js

---
+++ Chapter 3/Lesson 16 Website with HTML, CSS & JS/app.js
@@ -0,0 +1,7 @@
+    function handleCreatePost() {
+        document.getElementById("post-form-container").style.display = "block"
+    }
+
+    function hidePostForm() {
+        document.getElementById("post-form-container").style.display = "none"
+    }
